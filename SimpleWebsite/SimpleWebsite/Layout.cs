71c5b095f94cbb540a6c6630aaf5d997a7b196b1
Leandro Wajswajn <leandrowp@gmail.com>
2019-10-24 18:59:09 -0300

    SimpleApp

---
+++ SimpleWebsite/SimpleWebsite/Layout.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebsite
+{
+    public class Layout
+    {
+        public string BackgroundColor { get; set; }
+    }
+}
---
+++ SimpleWebsite/SimpleWebsite/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SimpleWebsite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public Layout layout { get; set; }
+
+        private readonly IOptions<Layout> _layout;
+
+        public IndexModel(IOptions<Layout> layout)
+        {
+            _layout = layout;
+        }
+
+        public void OnGet()
+        {
+            layout = _layout.Value;
+        }
+    }
+}
